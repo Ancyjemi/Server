0807ec78fff78b1badafa0cbff440382ff66f5cd
Ancyjemi <132878652+Ancyjemi@users.noreply.github.com>
2024-09-24 10:42:19 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const dbconn = require("./config/db");
-const Projects = require("./models/projectModel");
-require("dotenv").config();
-const port = process.env.PORT || 7778;
+const dbConn = require("./config/db");
+const Projects = require("./routes/projectRoute");
 app.use(express.json());
+
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+const port = process.env.PORT || 7778;
+app.use("/projects", Projects);
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome" });
+  res.status(400).json("Welcome");
 });
+
 app.listen(port, () => {
   console.log(`Server running in : ${port}`);
 });
